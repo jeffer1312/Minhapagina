6e464fe56cbebca574a675e34fedcfa19bd5aa44
jefferson felizardo <jefferson@iMac-de-jefferson.local>
2019-10-11 12:26:31 -0300

    teste de branch

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.mobile i').click(function() {
+        var el = $(this).parent().find('ul');
+        if (el.is(':visible') == false) {
+            el.fadeIn();
+        } else {
+            el.fadeOut();
+        }
+
+
+    })
+})
