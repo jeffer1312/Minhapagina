31a0862e9df495d87ced8bbe68a18fdbdcf63d34
jefferson felizardo <jefferson@iMac-de-jefferson.local>
2019-10-11 12:07:10 -0300

    primeiro uso do git

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Projeto 06</title>
+</head>
+
+<body>
+
+    <!-- icons social -->
+    <section class="main">
+        <div class="layer"></div>
+        <div class="center">
+            <header>
+                <div class="icons-social">
+                    <a href="https:facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https:instagram.com"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+                <div class="clear"></div>
+                <div class="logo">
+                    <img src="./images/logobranca1.png">
+
+                </div>
+
+
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">sobre</a></li>
+                        <li><a href="">contato</a></li>
+                    </ul>
+                </nav>
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">sobre</a></li>
+                        <li><a href="">contato</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+
+            </header>
+            <div class="conteudo-header">
+                <h2>Bem Vindo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec urna lectus. Vivamus pulvinar urna vel est eleifend venenatis. Aliquam eu pulvinar lectus, et pulvinar ex. Quisque leo justo, mollis ut eros quis, pellentesque finibus
+                    est. Donec eu sem nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis rutrum vitae dolor vitae feugiat. Duis pretium velit vel magna tincidunt laoreet. Curabitur pulvinar porttitor lorem. Nam vestibulum porttitor
+                    dui, quis fringilla sem tristique eget. Aliquam congue luctus sagittis. Nunc egestas metus eu magna rhoncus, nec eleifend dolor maximus.</p>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <!-- center -->
+    </section>
+    <!-- main -->
+    <section class="planos">
+        <div class="center">
+            <div class="chamada-planos">
+                <h2>Confira Nossos Planos</h2>
+
+            </div>
+            <!-- chamada planos -->
+            <section class="box-planos">
+                <div class="center">
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                            <h2>lorem ipsum</h2>
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i> lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                            </div>
+                            <!-- pontos plano -->
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                        </div>
+                        <!-- planos wraper -->
+                    </div>
+                    <!-- planos single -->
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                            <h2>lorem ipsum</h2>
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                            </div>
+                            <!-- pontos plano -->
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- planos single -->
+                    <div class="box-planos-single">
+                        <div class="box-planos-wraper">
+                            <h2>lorem ipsum</h2>
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i> lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>lorem ipsum silor domor</p>
+                            </div>
+                            <!-- pontos plano -->
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                            <!-- preco -->
+                        </div>
+                    </div>
+                    <!-- planos single -->
+                </div>
+                <!-- center -->
+            </section>
+            <!-- box planos -->
+        </div>
+        <!-- center -->
+    </section>
+    <section class="parallax">
+        <div class="overlay-parallax"></div>
+        <div class="center">
+            <h2>Sobre Nossa <span style="color: #2692ff;">Empresa</span></h2>
+            <p>Aliquam congue, ante ut feugiat pellentesque, nulla risus semper ipsum, ut dictum tortor urna vel elit. Nulla fermentum at purus condimentum egestas. Aliquam feugiat dui eros, in luctus nisi semper ut. Mauris tellus augue, viverra in viverra
+                non, malesuada vel enim. Nunc sit amet tempus est. Maecenas mattis efficitur dui, sit amet pretium erat auctor quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur id cursus justo. Nam
+                elementum hendrerit augue a lacinia. Morbi lacinia urna ac ullamcorper aliquet. Fusce ac mi in turpis maximus volutpat. Cras urna lacus, vestibulum in nunc a, rhoncus interdum mi. Integer sed condimentum est. Curabitur eu sem congue, volutpat
+                mauris in, ultricies felis.</p>
+            <p>Aliquam congue, ante ut feugiat pellentesque, nulla risus semper ipsum, ut dictum tortor urna vel elit. Nulla fermentum at purus condimentum egestas. Aliquam feugiat dui eros, in luctus nisi semper ut. Mauris tellus augue, viverra in viverra
+                non, malesuada vel enim. Nunc sit amet tempus est. Maecenas mattis efficitur dui, sit amet pretium erat auctor quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur id cursus justo. Nam
+                elementum hendrerit augue a lacinia. Morbi lacinia urna ac ullamcorper aliquet. Fusce ac mi in turpis maximus volutpat. Cras urna lacus, vestibulum in nunc a, rhoncus interdum mi. Integer sed condimentum est. Curabitur eu sem congue, volutpat
+                mauris in, ultricies felis.</p>
+        </div>
+
+
+    </section>
+    <section class="final-section">
+        <div class="center">
+            <div class="w50 contato">
+
+                <h2>
+                    <div class="borda-efeito"></div>Entre em contato</h2>
+                <form>
+                    <input type="text" placeholder="Insira seu nome...">
+                    <input type="text" placeholder="Insira seu email...">
+                    <textarea placeholder="Sua Mensagem..."></textarea>
+                    <input type="submit" value="Enviar!">
+                </form>
+            </div>
+            <div class="w50 time">
+                <h2>
+                    <div class="borda-efeito"></div>Conheça nosso time</h2>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight: bold;">Jefferson Felizardo</p>
+                            <p style="font-weight: italic;">CEO/ Programador</p>
+                        </div>
+                    </div>
+                    <!-- topo-box usuario -->
+                    <div class="descricao-usuario-box">
+                        <p>
+                            Aliquam congue, ante ut feugiat pellentesque, nulla risus semper ipsum, ut dictum tortor urna vel elit. Nulla fermentum at purus condimentum egestas. Aliquam feugiat dui eros, in luctus nisi semper ut. Mauris tellus augue, viverra in viverra non, malesuada
+
+                        </p>
+                    </div>
+                </div>
+                <!-- equipe single -->
+
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+    <footer>
+        <p>todos os direitos reservados</p>
+    </footer>
+</body>
+<script src="js/jquery.js"></script>
+<script src="js/functions.js"></script>
+
+
+</html>
